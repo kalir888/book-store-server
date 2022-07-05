5c46e9550399166c51b86afde564fdf058f2e2aa
Kalir888 <kalir888@gmail.com>
2022-07-05 19:18:37 +0530

    updatecart api added successfully

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -16,3 +16,35 @@ export const addToCart = async (req, res, next) => {
       });
     }
 };
+
+export const getCart = async (req, res, next) => {
+    try {
+      const data = await CartService.getCart(req.body.userId);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Cart fetched successfully'
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        code: HttpStatus.BAD_REQUEST,
+        message: `${error}`
+      });
+    }
+};
+
+export const updateCart = async (req, res, next) => {
+    try {
+      const data = await CartService.updateCart(req.params._id, req.body.userId, req.body.quantity);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'Cart updated successfully'
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        code: HttpStatus.BAD_REQUEST,
+        message: `${error}`
+      });
+    }
+};
