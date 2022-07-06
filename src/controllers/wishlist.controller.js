1c4f5630c0e243d6050017a8e1c9184c34e0ae99
Kalir888 <kalir888@gmail.com>
2022-07-06 21:40:29 +0530

    merged with getwishlist

--- src/controllers/wishlist.controller.js
+++ src/controllers/wishlist.controller.js
@@ -1,13 +1,29 @@
 import HttpStatus from 'http-status-codes';
-import * as wishListService from '../services/wishlist.service';
+import * as WishListService from '../services/wishlist.service';
 
 export const addToWishList = async (req, res, next) => {
     try {
-      const data = await wishListService.addToWishList(req.params._id, req.body.userId);
+      const data = await WishListService.addToWishList(req.params._id, req.body.userId);
       res.status(HttpStatus.CREATED).json({
         code: HttpStatus.CREATED,
         data: data,
         message: 'Book added to the WishList successfully'
+      });
+    } catch (error) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        code: HttpStatus.BAD_REQUEST,
+        message: `${error}`
+      });
+    }
+};
+
+export const getWishList = async (req, res, next) => {
+    try {
+      const data = await WishListService.getWishList(req.body.userId);
+      res.status(HttpStatus.OK).json({
+        code: HttpStatus.OK,
+        data: data,
+        message: 'WishList fetched successfully'
       });
     } catch (error) {
       res.status(HttpStatus.BAD_REQUEST).json({
--- src/routes/wishlist.route.js
+++ src/routes/wishlist.route.js
@@ -4,6 +4,8 @@ import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
+router.get('', userAuth, wishListController.getWishList);
+
 router.post('/:_id', userAuth, wishListController.addToWishList);
 
 export default router;
